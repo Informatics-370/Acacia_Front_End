ceb4e15391a022a8229d7b721d7f34d43ce34b40
u20502509 <u20502509@tuks.co.za>
2023-09-28 22:29:13 +0200

    Report Styling :)

--- src/app/manager/dashboard.component.ts
+++ src/app/manager/dashboard.component.ts
@@ -80,20 +80,20 @@ constructor(private companyService: CompanyService, private reportService: Repor
           label: 'Sales Report',
           data: this.Report.data,
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(66, 107, 105, 0.7)',
+            'rgba(139, 177, 116, 0.7)',
+            'rgba(34, 46, 80, 0.7)',
+            'rgba(181, 202, 141, 0.7)',
+            'rgba(42, 72, 73, 0.7)',
+            'rgba(169, 146, 125, 0.7)'
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'rgba(66, 107, 105, 1)',
+            'rgba(139, 177, 116, 1)',
+            'rgba(34, 46, 80, 1)',
+            'rgba(181, 202, 141, 1)',
+            'rgba(42, 72, 73, 1)',
+            'rgba(169, 146, 125, 1)'
           ],
           borderWidth: 1
         }]
